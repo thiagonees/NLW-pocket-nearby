a986778a5cf6259a568e93557cc3f1fa71c78eca
thiagonees <thiagoalves.devp@gmail.com>
2024-12-10 13:56:55 -0300

    ✨ Create button component

--- mobile/src/app/index.tsx
+++ mobile/src/app/index.tsx
@@ -2,17 +2,18 @@ import { View, Text } from "react-native"
 import React from 'react'
 import  Welcome  from "@/components/welcome"
 import { Steps } from "@/components/steps"
-
+import { Button } from "@/components/button"
+import { IconPlus } from "@tabler/icons-react-native"
 export default function Index() {
   return (
     <View style={{
       flex: 1, padding: 40, gap: 40
-      // justifyContent: 'center',
-      // alignItems: 'center'
     }}>
-
       <Welcome />
       <Steps />
+      <Button>
+        <Button.Title>Começar</Button.Title>
+      </Button>
     </View>
   )
 }
---
+++ mobile/src/components/button/styles.ts
@@ -0,0 +1,21 @@
+import { StyleSheet } from "react-native"
+import { colors } from "@/styles/colors"
+import { fontFamily } from "@/styles/font-family"
+
+export const s = StyleSheet.create({
+  container: {
+    height: 56,
+    maxHeight: 56,
+    backgroundColor: colors.green.base,
+    borderRadius: 10,
+    alignItems: "center",
+    justifyContent: "center",
+    flexDirection: "row",
+    gap: 14,
+  },
+  title: {
+    color: colors.gray[100],
+    fontFamily: fontFamily.semiBold,
+    fontSize: 16,
+  }
+})
